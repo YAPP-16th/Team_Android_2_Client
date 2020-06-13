35b0b38444a94eaca962e35686801e86e8c3240e
Lee Oh Hyoung <lee2928030@naver.com>
2020-06-13 14:44:17 +0900

    팀 생성 카드 ViewHolder 분리 (#54)

--- app/src/main/java/kr/yapp/teamplay/presentation/myteam/MyTeamAdapter.kt
+++ app/src/main/java/kr/yapp/teamplay/presentation/myteam/MyTeamAdapter.kt
@@ -6,31 +6,64 @@ package kr.yapp.teamplay.presentation.myteam
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import kr.yapp.teamplay.R
 import kr.yapp.teamplay.databinding.RvItemMyTeamBinding
+import kr.yapp.teamplay.databinding.RvItemTeamCreateBinding
 import kr.yapp.teamplay.domain.entity.MyTeam
+import kr.yapp.teamplay.util.dpToPixel
+import org.jetbrains.anko.windowManager
 
 class MyTeamAdapter(
     private val onCardClick: () -> Unit = {},
     private val onCardClickToGoTeamMain: (id: Int, position : Int) -> Unit
-) : RecyclerView.Adapter<MyTeamAdapter.MyTeamViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    enum class ViewType {
+        TEAM, CREATE
+    }
 
     private val teams: ArrayList<MyTeam> = arrayListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyTeamViewHolder =
-        MyTeamViewHolder(DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.rv_item_my_team,
-            parent,
-            false
-        ))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            ViewType.TEAM .ordinal -> {
+                MyTeamViewHolder(
+                    RvItemMyTeamBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+            ViewType.CREATE.ordinal -> {
+                TeamCreateViewHolder(
+                    RvItemTeamCreateBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+            else -> throw IllegalArgumentException("Un Support ViewType")
+        }
+    }
 
-    override fun onBindViewHolder(holder: MyTeamViewHolder, position: Int) =
-        holder.bindTo(teams[position], position)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder.itemViewType) {
+            ViewType.TEAM.ordinal -> {
+                (holder as MyTeamViewHolder).bindTo(teams[position], teams[position].id.toInt())
+            }
+            ViewType.CREATE.ordinal -> {
+                (holder as TeamCreateViewHolder).bindTo()
+            }
+        }
+    }
 
     override fun getItemCount(): Int = teams.count()
+
+    override fun getItemViewType(position: Int): Int {
+        return if(teams[position].isCreateCard) ViewType.CREATE.ordinal else ViewType.TEAM.ordinal
+    }
 
     fun updateMyTeam(list: List<MyTeam>) {
         teams.clear()
@@ -44,20 +77,24 @@ class MyTeamAdapter(
 
         private val displayMetrics = DisplayMetrics()
 
-        fun bindTo(myTeam: MyTeam, position: Int) {
+        fun bindTo(myTeam: MyTeam, id: Int) {
             binding.myTeam = myTeam
-//            (itemView.context as AppCompatActivity).windowManager.defaultDisplay.getMetrics(displayMetrics)
-//            itemView.layoutParams.width = (displayMetrics.widthPixels.toDouble() - 64.dpToPixel()).toInt()
+            binding.root.setOnClickListener { onCardClickToGoTeamMain(id, adapterPosition) }
+            itemView.context.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            itemView.layoutParams.width = (displayMetrics.widthPixels.toDouble() - 64.dpToPixel()).toInt()
+        }
+    }
 
-            if(myTeam.isCreateCard) {
-                binding.rvMyTeamRootLayout.setOnClickListener {
-                    onCardClick()
-                }
-            } else {
-                binding.rvMyTeamRootLayout.setOnClickListener {
-                    onCardClickToGoTeamMain(myTeam.id.toInt(), position)
-                }
-            }
+    inner class TeamCreateViewHolder(
+        private val binding: RvItemTeamCreateBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+
+        private val displayMetrics = DisplayMetrics()
+
+        fun bindTo() {
+            binding.root.setOnClickListener { onCardClick() }
+            itemView.context.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            itemView.layoutParams.width = (displayMetrics.widthPixels.toDouble() - 64.dpToPixel()).toInt()
         }
     }
 }
