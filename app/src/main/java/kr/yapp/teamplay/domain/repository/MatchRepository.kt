338f284f22bb719a6d38289c47c3a4c2dad393c5
Lee Oh Hyoung <lee2928030@naver.com>
2020-06-14 19:55:42 +0900

    이전 PR에서 빠진 RequestMatch API를 추가 (#63)

--- app/src/main/java/kr/yapp/teamplay/domain/repository/MatchRepository.kt
+++ app/src/main/java/kr/yapp/teamplay/domain/repository/MatchRepository.kt
@@ -1,6 +1,8 @@
 package kr.yapp.teamplay.domain.repository
 
+import io.reactivex.Completable
 import io.reactivex.Single
+import kr.yapp.teamplay.data.match.CreateMatchRequest
 import kr.yapp.teamplay.data.match.MatchListResponse
 import kr.yapp.teamplay.domain.entity.matchresult.DetailedMatchResult
 import kr.yapp.teamplay.domain.entity.matchresult.MatchIndividualScore
@@ -18,5 +20,7 @@ interface MatchRepository {
     fun getDetailedMatchResult(matchId: Int): Single<DetailedMatchResult>
 
     fun getDetailedMatchIndividualResult(matchId: Int): Single<List<MatchIndividualScore>>
+
+    fun requestMatch(createMatchRequest: CreateMatchRequest, matchId: Int): Completable
 
 }
